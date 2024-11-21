abe6907737f6f71b74c2e49d1bd280863deeca4a
Sopraj1120 <manchukansopraj@gmail.com>
2024-11-21 10:31:20 +0530

    Add ol for user

---
+++ HRMS/Controllers/UserOlevelController.cs
@@ -0,0 +1,42 @@
+﻿using HRMS.DTOs.RequestDtos;
+using HRMS.Iservice;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace HRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserOlevelController : ControllerBase
+    {
+        private readonly IUserOlevelService _userservice;
+
+        public UserOlevelController(IUserOlevelService userservice)
+        {
+            _userservice = userservice;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOlevel(Guid Id, UserOlevelRequestDtos userOlevelRequestDtos)
+        {
+            var data = await _userservice.AddOlevels(Id, userOlevelRequestDtos);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOlevelByUserId(Guid Id)
+        {
+            var data = await _userservice.GetOlevelByUserId(Id);
+
+            return Ok(data);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOlevelS(Guid Id)
+        {
+            await _userservice.DeleteOlevel(Id);
+            return NoContent();
+        }
+    }
+}
---
+++ HRMS/Repository/UserOlevelRepository.cs
@@ -0,0 +1,42 @@
+﻿using HRMS.DataBase;
+using HRMS.Entities;
+using HRMS.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS.Repository
+{
+    public class UserOlevelRepository : IUserOlevelRepo
+    {
+
+        private readonly HRMDBContext _context;
+
+        public UserOlevelRepository(HRMDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserOLevel> AddOlevel(UserOLevel userOLevel)
+        {
+            var data = await _context.userOLevels.AddAsync(userOLevel);
+            await _context.SaveChangesAsync();
+            return userOLevel;
+        }
+
+        public async Task<List<UserOLevel>> GetOlevelByUserId(Guid userId)
+        {
+            var data = await _context.userOLevels.Where(o => o.UserId == userId).ToListAsync();
+            return data;
+        }
+
+        public async Task DeleteOlevel(Guid Id)
+        {
+            var data = await _context.userOLevels.FindAsync(Id);
+            if (data != null)
+            {
+                _context.userOLevels.Remove(data);
+                await _context.SaveChangesAsync();
+
+            }
+        }
+    }
+}
